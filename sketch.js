97cc42b881a2dc5b85a770c91041abb1b36ac236
rdsalangsang <rosa.salangsang@sjsu.edu>
2021-11-08 12:05:22 -0800

    3.5

--- sketch.js
+++ sketch.js
@@ -8,17 +8,20 @@ function setup() {
 
 function draw() {
 if(mouseIsPressed){
-    line(width-mouseX, height-mouseY,width-pmouseX,height-pmouseY);
-    line(mouseX, mouseY,pmouseX,pmouseY);
-    array.push([mouseX,mouseY])
+    //line(width-mouseX, height-mouseY,width-pmouseX,height-pmouseY);
+    //line(mouseX, mouseY,pmouseX,pmouseY);
+    array.push([mouseX,mouseY]);
 }
 }
 function keyTyped(){
   if(key==='s'){
     saveCanvas('zen','png')
-  } else if (key==='e'){
-    clear();
-    background(220);
   }
+
+    else if(key==='d'){
+      for(let i = 0; i<array.length-1;i++){
+    line(array[i][0],array[i][1],array[i+1][0],array[i+1][1]);
+  }
+}
 return false;
 }
