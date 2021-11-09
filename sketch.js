1c1c64203864e4e930b024da63e2d19e0836ea1d
rdsalangsang <rosa.salangsang@sjsu.edu>
2021-11-08 22:55:28 -0800

    3.7

--- sketch.js
+++ sketch.js
@@ -1,36 +1,37 @@
-let array=[];
+let array = [];
 
 function setup() {
   createCanvas(600, 600);
-  background(255, 247, 158);
-    strokeWeight(4);
-    noFill();
+  background(240, 240, 228);
+  strokeWeight(4);
+  noFill();
 }
 
 function draw() {
-if(mouseIsPressed){
+  if (mouseIsPressed) {
     //line(width-mouseX, height-mouseY,width-pmouseX,height-pmouseY);
     //line(mouseX, mouseY,pmouseX,pmouseY);
     background(0);
-    text('breathe in...then out',300,300);
+    text('Breathe in...and then out.', 300, 300);
+    text('Think not of its perfection.', 300, 350);
+    text('But the art process.',300,400)
     fill(220);
-    array.push([mouseX,mouseY]);
-}
-}
-function keyTyped(){
-  if(key==='s'){
-    saveCanvas('zen','png')
+    array.push([mouseX, mouseY]);
   }
+}
 
-    else if(key==='d'){
-background(255, 247, 158);
-noFill();
-beginShape();
-      for(let i = 0; i<array.length;i++){
-    //line(array[i][0],array[i][1],array[i+1][0],array[i+1][1]);
-    curveVertex(array[i][0],array[i][1])
+function keyTyped() {
+  if (key === 's') {
+    saveCanvas('zen', 'png')
+  } else if (key === 'd') {
+    background(240, 240, 228);
+    noFill();
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
+      //line(array[i][0],array[i][1],array[i+1][0],array[i+1][1]);
+      curveVertex(array[i][0], array[i][1])
+    }
+    endShape();
   }
-  endShape();
-}
-return false;
+  return false;
 }
