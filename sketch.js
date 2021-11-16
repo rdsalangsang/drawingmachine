e3b340c19f6b728029ea712d751d5b985d10aded
rdsalangsang <rosa.salangsang@sjsu.edu>
2021-11-16 13:02:50 -0800

    final

--- sketch.js
+++ sketch.js
@@ -1,19 +1,21 @@
 let noiseOff = 0.0;
-let strokeWidth = 5;
+let strokeWidth = 10;
 
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(650, 650);
   strokeWeight(strokeWidth);
-  stroke(51);
+  ///stroke(51);
+  smooth();
 }
 
 function draw() {
   if (mouseIsPressed){
-  stroke(map(mouseX, 0, 650, 0, 130, true));
+  stroke(map(mouseX, 0, 600, 0, 100, true));
   strokeWeight(strokeWidth);
-  noiseOff += 0.03;
-  strokeWidth = noise(noiseOff) * 65
+  noiseOff += 0.06;
+  strokeWidth = noise(noiseOff) * 65;
   line(mouseX, mouseY, pmouseX, pmouseY);
+  blendMode(BLEND);
 }
 }
 function keyTyped() {
@@ -23,5 +25,6 @@ function keyTyped() {
   else if (key === 'e') {
     clear();
   }
+
   return false;
 }
